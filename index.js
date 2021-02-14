e3f2a2f03c2d3b6cf27962a6c536d68c99f18944
Samuel Wilson <samwil56@gmail.com>
2021-02-14 20:20:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musician, instrument) {
+  str = []
+  for (i=0,i<=musician.length,i++) {
+    plays = "{musician[i]} plays {instument[i]}."
+    str.push(plays)
+  }
+  return str
+}
