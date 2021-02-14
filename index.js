ac8706fb2fd304e758ee04a17669aa77df4141cd
Samuel Wilson <samwil56@gmail.com>
2021-02-14 20:32:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // add solution here
 function theBeatlesPlay(musician, instrument) {
   var str = []
-  for (i=0,i==musician.length,i++) {
+  var i = 0
+  for (i=0,i<=musician.length,i++ ) {
     player = musician[i]
     inst = instrument[i]
     plays = `{player} plays {inst}.`
