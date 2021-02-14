575520ae7c026f9cc1e2dca01ebd49bce23290ec
Samuel Wilson <samwil56@gmail.com>
2021-02-14 20:24:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 function theBeatlesPlay(musician, instrument) {
   str = ()
   for (i=0,i<=musician.length,i++) {
-    plays = `{musician[i]} plays {instument[i]}.`
+    player = musician[i]
+    inst = instrument[i]
+    plays = `{player} plays {inst}.`
     str.push(plays)
   }
   return str
